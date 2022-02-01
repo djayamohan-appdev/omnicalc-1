de206efabb8c439b9c0bb2a805fb6af601114f1d
djayamohan <djayamoh@chicagobooth.edu>
2022-02-01 23:58:16 +0000

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,36 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb"})
+  end
+
+  def calculate_square
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num ** 2
+
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb"})
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/random_results.html.erb"})
+  end
+
+  def blank_root_form
+    render({ :template => "calculation_templates/root_form.html.erb"})
+  end
+
+  def calculate_root
+    @num = params.fetch("user_num").to_f
+    @result = @num ** 0.5
+
+    render({ :template => "calculation_templates/root_results.html.erb"})
+  end
 end
